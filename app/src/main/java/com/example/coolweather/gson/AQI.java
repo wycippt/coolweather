650b6f9707eaeb00784acb0c7dc5effe4e9407ab
Leo <Leo@gmail.com>
2017-12-21 15:56:06 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.example.coolweather.gson;
+
+/**
+ * Created by Administrator on 2017/12/18.
+ */
+
+public class AQI {
+    public AQIcity city;
+    public class AQIcity{
+        public String aqi;
+        public String pm25;
+    }
+}
---
+++ app/src/main/java/com/example/coolweather/gson/Weather.java
@@ -0,0 +1,26 @@
+package com.example.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/12/20.
+ */
+
+public class Weather {
+
+    public String status;
+
+    public Basic basic;
+
+    public AQI aqi;
+
+    public Now now;
+
+    public Suggestion suggestion;
+
+    @SerializedName("daily_forecast")
+    public List<Forecast>forecastList;
+}
+ 
